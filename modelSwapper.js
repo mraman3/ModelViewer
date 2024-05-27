c33bd2dfe46d8d1e6edff3ab8641641d2912572b
Aman Braich <braich_aman3@yahoo.ca>
2024-05-27 16:36:00 -0400

    restructure assets and removed unnecessary folders

--- modelSwapper.js
+++ modelSwapper.js
@@ -1,14 +1,14 @@
 const modelViewerVariants = document.querySelector("model-viewer#shoe");
 
 document.getElementById("contactChoice1").addEventListener("click", function () {
-    modelViewerVariants.src = "models/DRIP1.glb";
+    modelViewerVariants.src = "assets/DRIP1.glb";
 }, false);
 
 document.getElementById("contactChoice2").addEventListener("click", function () {
-    modelViewerVariants.src = "models/DRIP2.glb";
+    modelViewerVariants.src = "assets/DRIP2.glb";
 }, false);
 
 document.getElementById("contactChoice3").addEventListener("click", function () {
-    modelViewerVariants.src = "models/DRIP3.glb";
+    modelViewerVariants.src = "assets/DRIP3.glb";
 }, false);
 
